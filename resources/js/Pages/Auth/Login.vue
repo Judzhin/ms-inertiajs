bd4bc71882fdf99f7512efbbf8d5b29c83b7835b
Judzhin Miles <info@msbios.com>
2021-11-23 18:39:32 +0200

    Add Login Form

---
+++ resources/js/Pages/Auth/Login.vue
@@ -0,0 +1,30 @@
+<template>
+
+    <Head>
+        <!--<title>My App - Home</title>-->
+        <title>Home</title>
+        <meta type="description" content="Information About My Home Page" head-key="description"/>
+    </Head>
+
+    <!--    <Layout>-->
+    <h1>Hello, {{ name }}</h1>
+    <p>In this series, we will use the followwing frameworks:</p>
+    <ul>
+        <li v-for="framework of frameworks" v-text="framework"></li>
+    </ul>
+    <!--    </Layout>-->
+</template>
+<script>
+import Layout from '../Shared/Layout';
+import {Head} from '@inertiajs/inertia-vue3';
+
+export default {
+    // components: { Layout },
+    components: {Head},
+    layout: Layout,
+    props: {
+        name: String,
+        frameworks: Array
+    }
+};
+</script>
