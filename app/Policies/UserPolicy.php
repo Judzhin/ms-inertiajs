5c87168eafe5c947b9dff83e915ae6f6a89c0651
Judzhin Miles <info@msbios.com>
2021-11-24 20:27:36 +0200

    Intermedaite commit

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -4,8 +4,13 @@ namespace App\Policies;
 
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Auth\Access\Response;
 use Illuminate\Support\Facades\Auth;
 
+/**
+ * Class UserPolicy
+ * @package App\Policies
+ */
 class UserPolicy
 {
     use HandlesAuthorization;
@@ -13,8 +18,8 @@ class UserPolicy
     /**
      * Determine whether the user can view any models.
      *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @return Response|bool
      */
     public function viewAny(User $user)
     {
@@ -24,9 +29,9 @@ class UserPolicy
     /**
      * Determine whether the user can view the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param User $model
+     * @return Response|bool
      */
     public function view(User $user, User $model)
     {
@@ -36,20 +41,20 @@ class UserPolicy
     /**
      * Determine whether the user can create models.
      *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @return Response|bool
      */
     public function create(User $user)
     {
-        return 'arnold@example.com' === $user->email;
+        return 'admin@example.com' === $user->email;
     }
 
     /**
      * Determine whether the user can update the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param User $model
+     * @return Response|bool
      */
     public function update(User $user, User $model)
     {
@@ -59,9 +64,9 @@ class UserPolicy
     /**
      * Determine whether the user can delete the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param User $model
+     * @return Response|bool
      */
     public function delete(User $user, User $model)
     {
@@ -71,9 +76,9 @@ class UserPolicy
     /**
      * Determine whether the user can restore the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param User $model
+     * @return Response|bool
      */
     public function restore(User $user, User $model)
     {
@@ -83,9 +88,9 @@ class UserPolicy
     /**
      * Determine whether the user can permanently delete the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\User  $model
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param User $model
+     * @return Response|bool
      */
     public function forceDelete(User $user, User $model)
     {
